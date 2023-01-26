c903176cb832a9e1a9be200ad86b0095fb4b4b35
ND Morsalin <morsalin056@gmail.com>
2023-01-26 09:08:45 -0800

    practice done

---
+++ problem1.js
@@ -0,0 +1,10 @@
+/*  Harry’s mom gave him money and asked him to buy some oranges and apples. Write a program to
+help Harry calculate how much money the shopkeeper will return.
+Input:
+The first line of the input is the taka Harry’s mom gave him.
+The second line is the cost of 1 kg of oranges and 1 kg of apples. */
+
+var totalMoney = 1000;
+var orangesAndApplePrice = 700;
+var returnPrice = totalMoney - orangesAndApplePrice
+console.log(returnPrice);
---
+++ problem4.js
@@ -0,0 +1,18 @@
+/* Sarah’s mother is teaching her mathematics. She gave Sarah a number and asked her what the
+remainder would be if she divided the number by 5. Help Sarah write the program.
+Input:
+The first line of the input contains the number.
+Output:
+Print the remainder.
+
+Practice Problem 4
+
+Sample Input:
+119
+
+Output:
+4 */
+
+var input = 119;
+var remainder = input % 5;
+console.log(remainder);
